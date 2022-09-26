541ced4335ad78495555fad9c6ae327b2e3c266f
EduardoMirandaz <eduardo.cmazevedo@gmail.com>
2022-09-26 15:28:55 -0300

    First commit

---
+++ Automacao-De-Gastos/scrapper.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+from selenium import webdriver # Realiza a interação com a página web
+from selenium.webdriver.support.select import Select # Seleciona as opções
+from selenium.webdriver.common.by import By # Procura tags e encontra seletores
+from selenium.webdriver.common.keys import Keys # Links e teclas
+
+#Instanciando o browser
+path_driver = '/home/draude/Documentos/Desenvolvimento/Minicurso-Semcomp-Dados/Automacao-De-Gastos/chromedriver_linux64/chromedriver'
+url = 'https://03felipesampaio.github.io'
+driver = webdriver.Chrome(path_driver)
+driver.get(url)
+
